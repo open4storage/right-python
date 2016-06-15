6564db2f35fd1f2b4f83a9393848fd922a7646be
Adrián Moreno <adrian@morenomartinez.com>
2016-06-15 16:24:11 +0200

    Removes useless line

--- right-python.bat
+++ right-python.bat
@@ -4,9 +4,10 @@ if errorlevel 1 goto errorNoPython
 
 for /f %%i IN ('python -c "import sys; print(sys.version_info[0])"') DO set pythonversion=%%i
 echo Python version found is: %pythonversion%
-start chrome "http://localhost:8000/login.html"
+
 if "%pythonversion%"=="2" echo run the python version2.py code here
 if "%pythonversion%"=="3" echo run the python version3.py code here
+
 goto:eof
 
 :errorNoPython
