082e45dd54c82efde8a61380ee863f04af4543f7
Adrian Moreno <adrian@morenomartinez.com>
2016-06-15 16:15:24 +0200

    Fixes code and checks if python exists

--- right-python.bat
+++ right-python.bat
@@ -1,7 +1,14 @@
 @echo off
-for /f "tokens=1,2,* delims=. " %%A in ('python -V') do set pythonversion=%%B
+python --version 2>NUL
+if errorlevel 1 goto errorNoPython
 
+for /f %%i IN ('python -c "import sys; print(sys.version_info[0])"') DO set pythonversion=%%i
 echo Python version found is: %pythonversion%
-
+start chrome "http://localhost:8000/login.html"
 if "%pythonversion%"=="2" echo run the python version2.py code here
 if "%pythonversion%"=="3" echo run the python version3.py code here
+goto:eof
+
+:errorNoPython
+echo.
+echo Error^: Python not installed
